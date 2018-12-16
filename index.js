94adb94862e662b39a370bbf54027726baf10d5b
JackyRecelis <jackymlui@live.com>
2018-12-16 20:22:40 +1000

    connected to mlab server

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require('express');
+require("dotenv").config();
 const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true});
+
+var db = mongoose.connection;
+
+db.on('error', (err)=>{
+    console.log("connection error", err);
+});
+
+db.once('open', ()=>{
+    console.log('connected');
+});
 
 const app = express();
 
