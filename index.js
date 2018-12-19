0d671345c1f0499fc91fa8ef7364512ec221fb1b
JackyRecelis <jackymlui@live.com>
2018-12-19 21:28:13 +1000

    creating schema and adding model (untested!)

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 require("dotenv").config();
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+let person = new Schema({
+    name: String,
+    age: String,
+    food: String,
+    favouriteFoods:[{food:String}]
+}) 
+
+let PersonModel = mongoose.model('PersonModel', person);
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true});
 
